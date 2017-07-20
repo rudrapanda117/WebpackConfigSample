d9e90f886adbffc7238fff8f3231d8c0a3392ef2
Rudrasish panda <rudrapanda117@gmail.com>
2017-07-20 16:35:58 +0530

    q

---
+++ src/main/resources/static/app/dist/lazyLoadingTemplateHtml.js
@@ -0,0 +1,10 @@
+webpackJsonp([5],{
+
+/***/ 175:
+/***/ (function(module, exports) {
+
+module.exports = "lazyLoadingTemplate - {{llt.demoModel}}";
+
+/***/ })
+
+});
---
+++ src/main/resources/static/app/dist/usingbundleloaderModule.js
@@ -0,0 +1,14 @@
+webpackJsonp([1],{
+
+/***/ 173:
+/***/ (function(module, exports) {
+
+angular.module('usingbundleloaderModule',[])
+.component('usingbundleloaderComponent', {
+  bindings: { user: '<' },
+  template: ` <h3>User usingbundleloaderModule {{ $ctrl.user.id }}</h3>`
+})
+
+/***/ })
+
+});
